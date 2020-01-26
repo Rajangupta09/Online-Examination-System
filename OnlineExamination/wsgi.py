4ef3aa93774be4e45acd24d531e35b9574b44945
Rajangupta09 <rjnkumar05@gmail.com>
2020-01-26 20:40:43 +0530

    commit

--- OnlineExamination/wsgi.py
+++ OnlineExamination/wsgi.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineExamination.settings')
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineExamination.settings')
+
 application = get_wsgi_application()
