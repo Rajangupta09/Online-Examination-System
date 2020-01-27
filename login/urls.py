bff5f76631a15d96a04d1858f89b5c4a6840cd66
Rajangupta09 <rjnkumar05@gmail.com>
2020-01-28 02:07:44 +0530

    commit

--- login/urls.py
+++ login/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.conf import settings
+from django.forms import ModelForm
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login/login.html'),name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='login/logout.html') ,name='logout'),
     path('login/dashboard/', views.dashboard ,name='home'),
-    path('login/category/', views.category ,name='category'),
+    path('login/category/', views.category ,name='category'), 
+    path('login/sub_category/', views.sub_category ,name='sub_category'), 
+    path('delete/<int:pk>', views.CategoryDelete , name='Category_delete'),
+    path('delete/<int:pk>', views.SubCategoryDelete , name='Sub_Category_delete'),
+
 ]    
 
 if settings.DEBUG:
--- login/views.py
+++ login/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse, get_object_or_404, redirect
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate ,login
@@ -34,13 +34,31 @@ def category(request):
 		categories_list = Categories.objects.all().order_by('-date_created')
 		return render(request, 'login/category.html',{'categories':categories_list}) 
 
+
 def sub_category(request):
 	if(request.method=="POST"):
 		post=sub_categories()
 		post.sub_category=request.POST.get('sub_category')
+		post.category_id=request.POST.get('category_id')
 		post.save()
+		categories_list = Categories.objects.all().order_by('-date_created')
 		sub_categories_list = sub_categories.objects.all().order_by('-date_created')
-		return render(request, 'login/sub_category.html',{'sub_categories':sub_categories_list})  
+		return render(request, 'login/sub_category.html',{'categories':categories_list,'sub_categories':sub_categories_list,})  
 	else:
 		sub_categories_list = sub_categories.objects.all().order_by('-date_created')
-		return render(request, 'login/sub_category.html',{'sub_categories':sub_categories_list})          
+		categories_list = Categories.objects.all().order_by('-date_created')
+		return render(request, 'login/sub_category.html',{'categories':categories_list,'sub_categories':sub_categories_list,})          
+
+def CategoryDelete(request, pk,):
+    cdelete = get_object_or_404(Categories, pk=pk)    
+    if request.method=='POST':
+        cdelete.delete()
+        return redirect('category')
+    return render(request, 'login/category.html', {'cdelete': cdelete})
+
+def SubCategoryDelete(request, pk,):
+    cdelete = get_object_or_404(sub_categories, pk=pk)    
+    if request.method=='POST':
+        cdelete.delete()
+        return redirect('sub_category')
+    return render(request, 'login/sub_category.html', {'cdelete': cdelete})
